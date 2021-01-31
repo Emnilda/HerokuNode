cc1c5002379ef02af1a5b213d1dee207fc752527
Emnilda <sandra.czolczynska@gmail.com>
2021-01-31 13:35:05 +0100

    intitial

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express"); 
+const port = process.env.PORT || 4000;
+const app = express();
+
+
+app.set("view engine", 'hbs');
+
+const path = require('path')
+app.use('/assets', express.static(path.join(__dirname, "./assets")));
+app.use('/js', express.static(path.join(__dirname, "./js")));
+app.get('/', function (req, res) {
+res.render('index', {
+    pageTitle: "Lekcja z Nodem"})
+});
+
+app.get('/about', function (req, res){
+res.send("My site")
+})
+
+app.listen(port, (err) => {
+if (err) {
+return console.log("coś poszło nie tak...:", err)
+}
+console.log("serwer działa na porcie", port)
+})
---
+++ function.js
@@ -0,0 +1,22 @@
+const helloWorld = () => console.log("Hello, World!");
+
+   const add = (a,b) => console.log(a + b);   
+
+   module.exports ={
+       helloWorld,
+       add
+   }
+
+
+   console.log("Załadowano wszysgkie funkcje");
+
+   const sayHello = () => { console.log("Hello World") }
+const add = (a, b) => {
+console.log(a + b)
+}
+const someTitle = "example example" //kod do dopisania
+module.exports = {
+sayHello,
+add,
+someTitle //kod do dopisania
+}
